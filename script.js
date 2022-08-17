b86d98faf7511cbfede048e5aba7a0a535194c6f
Travis Umbel <Umbelth94@gmail.com>
2022-08-16 19:02:32 -0500

    Add game function that plays the game in multiple rounds
    Added a game function that plays through multiple rounds.  I also included
    a scoreboard, and a way to commit to playing however many rounds you want
    before starting the game.

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ let rockPaperScissors = [
     'rock','paper','scissors'
 ]
 
+let playerPoints = 0;
+let computerPoints = 0;
+let rounds = 0;
+let playerWin;
+
+function getScore(){
+    return `Round ${rounds}: Player has ${playerPoints}, Computer has ${computerPoints}`
+}
+
 
 function getComputerChoice(){
     let computerChoice = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)]
-    // console.log(computerChoice);
     return computerChoice;
 }
 
@@ -23,25 +31,35 @@ function playRound(playerSelection,computerSelection){
         || lowerCaseSelection == 'scissors'){
             //You can probably make this a seperate function to check if they've won, and make the playRound function less cluttered
             if (lowerCaseSelection == 'rock'){
+                rounds += 1;
                 if (computerSelection == 'rock'){
+                    
                     return 'It was a tie';
                 } else if (computerSelection == 'paper'){
+                    computerPoints += 1;
                     return 'Paper covers rock, you lose!';
                 } else if (computerSelection == 'scissors'){
+                    playerPoints += 1;
                     return 'Rock smashes scissors, you win!';
                 }
             } else if (lowerCaseSelection == 'paper'){
+                rounds += 1;
                 if (computerSelection == 'rock'){
+                    playerPoints += 1;
                     return 'Paper covers rock, you win!';
                 } else if (computerSelection == 'paper'){
                     return 'It was a tie';
                 } else if (computerSelection == 'scissors'){
+                    computerPoints += 1;
                     return 'Scissors cuts paper, you lose';
                 }
             } else if (lowerCaseSelection == 'scissors'){
+                rounds += 1;
                 if (computerSelection == 'rock'){
+                    computerPoints += 1;
                     return 'Your scissors got smashed, you lose';
                 } else if (computerSelection == 'paper') {
+                    playerPoints += 1;
                     return 'Your scissors slice the paper, you win!';
                 } else if (computerSelection == 'scissors'){
                     return 'Hot scissor action... Its a tie';
@@ -54,26 +72,26 @@ function playRound(playerSelection,computerSelection){
     } else {
         return 'That is not an option';
     }
-    //Create variable to store result as playerSelection
-    //Check to see if playerSelection is a string
-        //Lowercase each of the string options so that casing doesn't matter
-        //Check that it is either Rock, Paper, or Scissors
-            //If not, yell at them and ask them with a new prompt
-    //Get the computerselection variable to equal the result from getComputerChoice
-    //Compare each possible answer from each.
-        //If playerSlection = rock
-            //Rock vs Rock = tie
-            //Rock vs Paper = lose
-            //Rock vs Scissors = win
-        //If playerSelection = paper
-            //paper vs Rock = win
-            //paper vs paper = tie
-            //paper vs scissors = lose
-        //If playerSelection = scissors
-            //scissors vs rock = lose
-            //scissors vs paper = win
-            //scissors vs scissors = tie;
-    //Return result of game via string
 }
 
-console.log(playRound());
+function game(){
+    roundsToPlay = window.prompt('How many rounds would you like to play?');
+    console.log(`You are playing to ${roundsToPlay} rounds`);
+        for (let i = 0; i < roundsToPlay; i++){
+            playRound();
+            console.log(getScore());
+        }
+        if (playerPoints > computerPoints){
+            console.log('You win');
+            return 'Congratulations, you win';
+        } else if (playerPoints < computerPoints){
+            console.log('You lose');
+            return 'Oh shucks, you lose';
+        } else {
+            console.log('tie');
+            return 'Oh wow, it was a tie';
+        }
+   
+}
+
+console.log(game());
