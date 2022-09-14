f0a443c26434572a0b8dc2970e6914078439fbf3
Travis Umbel <Umbelth94@gmail.com>
2022-09-13 19:58:15 -0500

    Add Start Button and Reset button with CSS Toggling
    Added some CSS toggling functions to interact w/ different menus,
    and added functionality to determine how many rounds the player would
    like to play.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let rockPaperScissors = [
     'rock','paper','scissors'
 ]
+//Next Goals
+//Add an option to see a history of each round that can be toggled on/off with a button
+//Pretty up the buttons and UI.  Add style and maybe some effects/pictures
+//Hide all buttons at first, and then have them all show up when you hit "start game"
+    //Have a button or input that allows you to decide how many rounds you'd like to play to.
+    //Include a 'Play for me' button that will just play through the selected amount of rounds.
+
+
 
 const rockButton = document.querySelector('#rock');
 const paperButton = document.querySelector('#paper');
@@ -12,11 +20,13 @@ const displayResult = document.querySelector('#result');
 const displayRound = document.querySelector('#rounds');
 const displayComputerActions = document.querySelector('#computeractions');
 const displayPlayerActions = document.querySelector('#playeractions');
-const allDivs = document.querySelectorAll('div');
+const allStats = document.querySelectorAll('.stat'); //Have an issue with this not wanting to reset
+const roundsInput = document.querySelector('#roundstoplay')
 let playerPoints = 0;
 let computerPoints = 0;
 let rounds = 0;
 let playerWin;
+let roundsToPlay = 0;
 
 resetButton.addEventListener('click',() => {
     let confirmAction = confirm('Are you sure you want to reset scores?');
@@ -24,30 +34,37 @@ resetButton.addEventListener('click',() => {
     playerPoints = 0;
     computerPoints = 0;
     rounds = 0;
-    allDivs.forEach((div) => { 
-        div.textContent = ''}); //Makes all divs blank upon hitting reset button
-    } else {
+    allStats.forEach((div) => {
+        console.log(div); 
+        div.textContent = ''
+    }); //Makes all divs blank upon hitting reset button
+    toggleElement("roundsprompt"); //Brings back the roundsprompt menu
+    toggleElement("menu-container"); //hides the menu-buttons
+} else {
         return;
     }
 })
 
 rockButton.addEventListener('click', () => {
-    if ((playerPoints < 5) && (computerPoints < 5)){
+    if ((playerPoints < roundsToPlay) && (computerPoints < roundsToPlay)){ //Can probably reduce these lines of code into functions
     playRound('rock',getComputerChoice());
     displayScore();
     checkPoints(playerPoints,computerPoints); //Checks to see if either player's points are at 5.  Displays message if they are.
-    }});
+    console.log(playerPoints);
+    console.log
+}});
 
 paperButton.addEventListener('click',() =>{
-    if ((playerPoints < 5) && (computerPoints < 5)){
+    if ((playerPoints < roundsToPlay) && (computerPoints < roundsToPlay)){
     playRound('paper',getComputerChoice())
     displayScore();
     checkPoints(playerPoints,computerPoints);
     }});
 
-scissorsButton.addEventListener('click',() =>{
-    if ((playerPoints < 5) && (computerPoints < 5)){
+scissorsButton.addEventListener('click',(e) =>{
+    if ((playerPoints < roundsToPlay) && (computerPoints < roundsToPlay)){
     playRound('scissors',getComputerChoice())
+    console.log(e);
     displayScore();
     checkPoints(playerPoints,computerPoints);
     }});
@@ -78,51 +95,40 @@ function playRound(playerSelection,computerSelection){
                 rounds += 1;
                 if (computerSelection == 'rock'){
                     displayResult.textContent = 'It was a tie';
-                    // return 'It was a tie';
                 } else if (computerSelection == 'paper'){
                     computerPoints += 1;
                     displayResult.textContent = 'Paper covers rock, you lose!';
-                    // console.log('Paper covers rock, you lose!');
-                    // return 'Paper covers rock, you lose!';
                 } else if (computerSelection == 'scissors'){
                     playerPoints += 1;
                     displayResult.textContent = 'Rock smashes scissors, you win!';
-                    // console.log('Rock smashes scissors, you win!');
-                    // return 'Rock smashes scissors, you win!';
                 }
             } else if (playerSelection == 'paper'){
                 rounds += 1;
                 if (computerSelection == 'rock'){
                     playerPoints += 1;
                     displayResult.textContent = 'Paper covers rock, you win!';
-                    // return 'Paper covers rock, you win!';
                 } else if (computerSelection == 'paper'){
                     displayResult.textContent = 'It was a tie';
-                    // return 'It was a tie';
                 } else if (computerSelection == 'scissors'){
                     computerPoints += 1;
                     displayResult.textContent = 'Scissors cuts paper, you lose';
-                    // return 'Scissors cuts paper, you lose';
                 }
             } else if (playerSelection == 'scissors'){
                 rounds += 1;
                 if (computerSelection == 'rock'){
                     computerPoints += 1;
                     displayResult.textContent = 'Your scissors got smashed, you lose';
-                    // return 'Your scissors got smashed, you lose';
                 } else if (computerSelection == 'paper') {
                     playerPoints += 1;
                     displayResult.textContent = 'Your scissors slice the paper, you win!';
-                    // return 'Your scissors slice the paper, you win!';
                 } else if (computerSelection == 'scissors'){
                     displayResult.textContent = 'Hot scissor action... Its a tie';
-                    // return 'Hot scissor action... Its a tie';
                 }}  
             };
       
 
 function checkPoints(playerPoints,computerPoints){
-    if ((playerPoints == 5) || (computerPoints == 5)){
+    if ((playerPoints == roundsToPlay) || (computerPoints == roundsToPlay)){
         decideWinner();
     }
 }
@@ -135,23 +141,46 @@ function decideWinner(){
     }
 };
 
+//Utilize this function for when we want to do the 'auto rounds' player.  
+function game(){
+    roundsToPlay = window.prompt('How many rounds would you like to play?');
+    console.log(`You are playing to ${roundsToPlay} rounds`);
+        for (let i = 0; i < roundsToPlay; i++){
+            console.log(playRound());
+            console.log(getScore());
+        }
+        if (playerPoints > computerPoints){
+            return 'Congratulations, you win';
+        } else if (playerPoints < computerPoints){
+            return 'Oh shucks, you lose';
+        } else {
+            return 'Oh wow, it was a tie';
+        }   
+};
 
-// funcion game(){
-//     roundsToPlay = window.prompt('How many rounds would you like to play?');
-//     console.log(`You are playing to ${roundsToPlay} rounds`);
-//         for (let i = 0; i < roundsToPlay; i++){
-//             console.log(playRound());
-//             console.log(getScore());
-//         }
-//         if (playerPoints > computerPoints){
-//             return 'Congratulations, you win';
-//         } else if (playerPoints < computerPoints){
-//             return 'Oh shucks, you lose';
-//         } else {
-//             return 'Oh wow, it was a tie';
-//         }
 
-   
-// }
+function toggleElement(elementName){
+    let visibility =  document.defaultView.getComputedStyle(document.getElementById(elementName), null).getPropertyValue("visibility");;
+    if (visibility == "hidden"){
+        document.getElementById(elementName).style.visibility = "visible";
+    } else {
+        document.getElementById(elementName).style.visibility = "hidden";
+    }   
+};
+
+//Start Button that makes rest of buttons visible
+document.getElementById("start").addEventListener("click", () => {
+    console.log('start button clicked');
+    toggleElement("start");
+    toggleElement("roundsprompt");
+});
+
+roundsInput.addEventListener('keydown',(e) => {
+    if (e.key == 'Enter'){
+        roundsToPlay = roundsInput.value;
+        toggleElement("roundsprompt");
+        toggleElement("menu-container");
+    }
+});
 
 // console.log(g
